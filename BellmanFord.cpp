72b266511517d7a24a9c956e4182071c15efd24a
Andrew Yeh <ayeh6@ucmerced.edu>
2017-12-07 02:50:44 +0000

    gg

--- BellmanFord.cpp
+++ BellmanFord.cpp
@@ -3,71 +3,105 @@
 
 using namespace std;
 
-struct edge {
-	int u;
-	int v;
-	int w;
+struct edge
+{
+	int v1;
+	int v2;
+	int wgt;
 };
 
-struct graph {
-	struct edge* arr;
+struct graph
+{
+	struct edge* edges;
 };
 
-int main() {
-	int V, E, u, v, w;
-	cin >> V;
-	cin >> E;
-	//cout << "V: " << V << "E: " << E << endl;
-	struct graph* G = new graph;
-	int cost[V];
-	edge* edges = new edge[E];
-	bool cycleFound = false;
-	G->arr = edges;
-	for (int i = 0; i < E; i++) {
-		//cout << "loop: " << i << endl;
-		cin >> u;
-		cin >> v;
-		cin >> w;
-		G->arr[i].u = u;
-		G->arr[i].v = v;
-		G->arr[i].w = w;
-	}
 
-	for (int i = 0; i < V; i++) {
-		//cout << "loop2: " << i << endl;
-		if (i == 0)
-			cost[i] = 0;
-		else
-			cost[i] = INT_MAX;
-	}
-
-	for (int i = 1; i <= V-1; i++) {
-		//cout << "loop3: " << i << endl;
-		for (int j = 0; j < E; j++) {
-			//cout << "loop4: " << j << endl;
-			if (cost[G->arr[j].u] != INT_MAX && (cost[G->arr[j].u] + G->arr[j].w < cost[G->arr[j].v])) {
-				cost[G->arr[j].v] = cost[G->arr[j].u] + G->arr[j].w;
-			}
-		}
-	}
-
-	for (int i = 0; i < E; i++) {
-		//cout << "loop5: " << i << endl;
-		if (cost[G->arr[i].u] != INT_MAX && (cost[G->arr[i].u] + G->arr[i].w < cost[G->arr[i].v])) {
-			cycleFound = true;
-		}
-	}
-
-	if (cycleFound == true) {
+void print(int cost[], bool cycle, int numverts)
+{
+	if (cycle == true)
+	{
 		cout << "FALSE" << endl;
 	}
-	else if (cycleFound == false){
+        else if (cycle == false)
+	{
 		cout << "TRUE" << endl;
-		for (int i = 0; i < V; i++) {
-			if (cost[i] == INT_MAX)
-				cout << "INFINITY" << endl;
-			else
-				cout << cost[i] << endl;
-		}
+                for (int i = 0; i < numverts; i++)
+                {
+                        if (cost[i] == INT_MAX)
+                        {
+                                cout << "INFINITY" << endl;
+                        }
+                        else
+                        {
+                                cout << cost[i] << endl;
+                        }
+                }
+        }
+}
+
+bool BellmanFord(graph* G, int numverts, int numedges, int cost[])
+{
+	bool cycle = false;
+        for (int i = 0; i < numverts; i++)
+        {
+                if (i == 0)
+                {
+                        cost[i] = 0;
+                }
+                else
+                {
+                        cost[i] = INT_MAX;
+                }
+        }
+
+	for (int i = 1; i <= numverts-1; i++)
+	{
+	        for (int j = 0; j < numedges; j++)
+	        {
+	                if (cost[G->edges[j].v1] != INT_MAX && (cost[G->edges[j].v1] + G->edges[j].wgt < cost[G->edges[j].v2]))
+	                {
+	                        cost[G->edges[j].v2] = cost[G->edges[j].v1] + G->edges[j].wgt;
+	                }
+	        }
 	}
+
+	for (int i = 0; i < numedges; i++)
+	{
+	        if (cost[G->edges[i].v1] != INT_MAX && (cost[G->edges[i].v1] + G->edges[i].wgt < cost[G->edges[i].v2]))
+	        {
+	                cycle = true;
+	        }
+	}
+
+	print(cost,cycle,numverts);
+}
+
+int main()
+{
+	int numverts;
+	int numedges;
+
+	cin >> numverts;
+	cin >> numedges;
+
+	int v1;
+	int v2;
+	int wgt;
+	int cost[numverts];
+
+	struct graph* G = new graph;
+	edge* edges = new edge[numedges];
+	G->edges = edges;
+
+	for (int i = 0; i < numedges; i++)
+	{
+		cin >> v1;
+		cin >> v2;
+		cin >> wgt;
+		G->edges[i].v1 = v1;
+		G->edges[i].v2 = v2;
+		G->edges[i].wgt = wgt;
+	}
+
+	BellmanFord(G, numverts, numedges, cost);
 }
